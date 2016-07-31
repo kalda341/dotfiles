acafd467066c2e2a01675dcbd624f11b81f4015a
Max Lay <kalda341@gmail.com>
2016-07-31 23:53:26 +1200

    Fixed workspace scripts

--- desktop/i3/scripts/rename_workspace.py
+++ desktop/i3/scripts/rename_workspace.py
@@ -6,8 +6,9 @@ import json
 new_name = sys.stdin.readlines()[0]
 
 process = subprocess.Popen(["i3-msg", "-t", "get_workspaces"], stdout=subprocess.PIPE)
-output = str(process.communicate()[0])[1:].replace("\\n", "")
+output = str(process.communicate()[0])[1:].replace("\\n", "").replace("'", "")
+print(output)
 workspaces = json.loads(output)
 workspace = list(filter(lambda i: i['focused'] == True, workspaces))[0]
 
-test = subprocess.Popen(["i3-msg","rename worspace %s to %s" % (workspace['name'], new_name)], stdout=subprocess.PIPE)
+test = subprocess.Popen(["i3-msg","rename workspace %s to %s" % (workspace['name'], new_name)], stdout=subprocess.PIPE)
