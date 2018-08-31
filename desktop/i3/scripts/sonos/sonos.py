974c36c9179bc2dc4a8b906b51fe47e36a49b8a9
Max Lay <kalda341@gmail.com>
2018-09-01 11:16:35 +1200

    Cleaned up i3 config

---
+++ desktop/i3/scripts/sonos/sonos.py
@@ -0,0 +1,17 @@
+#!/bin/env python3
+import sys
+import soco
+
+if __name__ == '__main__':
+    device = soco.SoCo('192.168.1.47')
+
+    if sys.argv[1] in ['play', 'pause', 'stop', 'previous', 'next']:
+        getattr(device, sys.argv[1])()
+    elif sys.argv[1] == 'current':
+        track = device.get_current_track_info()
+        print('{} - {} - {} ({}%)'.format(track['title'], track['artist'], track['position'], device.volume))
+    elif sys.argv[1] == 'volume':
+        if sys.argv[2].startswith('+') or sys.argv[2].startswith('-'):
+            device.volume += int(sys.argv[2])
+        else:
+            device.volume = sys.argv[2]
